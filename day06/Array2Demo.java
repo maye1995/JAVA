fa6a3475b2572027897525ea887912afb27fcaf5
maye1995 <maye199512@gmail.com>
2017-03-16 23:56:31 +0800

    面向对象举例

--- day06/Array2Demo.java
+++ day06/Array2Demo.java
@@ -9,7 +9,7 @@ class Array2Demo
 
 
 		//int [ ] arr =new int [3];
-
+		int sum =0
 		int [][] arr=new int [3][2];//创建一个二维数组，该数组中有3个一维数组，每一个一维数组中有两个元素。
 
 
@@ -17,5 +17,22 @@ class Array2Demo
 		System.out.println(arr[0]);//直接打印二维数组中角标为0的一维数组
 		System.out.println(arr[0][0]);//直接打印二维数组中角标为0的一维数组中角标为0的元素
 
+		int  [] [] arr ={{3,1 },{3,2,4 },{3,9,0}};
+		for (int x=0;x<arr.length ;x++ )
+		{
+			for (int y=0;y<arr[x] ;y++ )
+			{
+					System.out.println(arr[x][y]+",");
+					sum =+arr[x][y];
+			}
+			
+		}
+			System.out.println("sum"+sum);
+
+
+
+//			甲：30,59,28,17
+	//			乙：37，60，22，19
+				int []arr = {{30,59,28,17},{37,60,22,19}};
 	}
 }
